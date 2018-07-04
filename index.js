ad46fd30c49a01f3792b89c1169e8f638839d9f9
marcodagolini <marco.dagolini@gmail.com>
2018-07-04 15:19:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,40 +38,42 @@ function download (content, filename, contentType) {
 
 function createExcel(){
 	// Require library
-var excel = require('excel4node');
+	var excel = require('excel4node');
 
 // Create a new instance of a Workbook class
-var workbook = new excel.Workbook();
+	var workbook = new excel.Workbook();
 
 // Add Worksheets to the workbook
-var worksheet = workbook.addWorksheet('Sheet 1');
-var worksheet2 = workbook.addWorksheet('Sheet 2');
+	var worksheet = workbook.addWorksheet('Sheet 1');
+	var worksheet2 = workbook.addWorksheet('Sheet 2');
 
 // Create a reusable style
-var style = workbook.createStyle({
-  font: {
-    color: '#FF0800',
-    size: 12
-  },
-  numberFormat: '$#,##0.00; ($#,##0.00); -'
-});
+	var style = workbook.createStyle({
+  	font: {
+    	color: '#FF0800',
+    	size: 12
+  	},
+  	numberFormat: '$#,##0.00; ($#,##0.00); -'
+	});
 
 // Set value of cell A1 to 100 as a number type styled with paramaters of style
-worksheet.cell(1,1).number(100).style(style);
+	worksheet.cell(1,1).number(100).style(style);
 
 // Set value of cell B1 to 300 as a number type styled with paramaters of style
-worksheet.cell(1,2).number(200).style(style);
+	worksheet.cell(1,2).number(200).style(style);
 
 // Set value of cell C1 to a formula styled with paramaters of style
-worksheet.cell(1,3).formula('A1 + B1').style(style);
+	worksheet.cell(1,3).formula('A1 + B1').style(style);
 
 // Set value of cell A2 to 'string' styled with paramaters of style
-worksheet.cell(2,1).string('string').style(style);
+	worksheet.cell(2,1).string('string').style(style);
 
 // Set value of cell A3 to true as a boolean type styled with paramaters of style but with an adjustment to the font size.
-worksheet.cell(3,1).bool(true).style(style).style({font: {size: 14}});
+	worksheet.cell(3,1).bool(true).style(style).style({font: {size: 14}});
 
-workbook.write('Excel.xlsx');
+	workbook.write('Excel.xlsx');
+	
+	return workbook;
 	
 	
 }
@@ -1391,7 +1393,7 @@ app.get('/download', function(req, res) {
 		}
 		console.log("finish!");
 		// var myURL = download (myResult, 'download.xls', 'application/vnd.ms-excel')
-		createExcel();
+		var myFile = createExcel();
 
 		res.send("done");
 	}
