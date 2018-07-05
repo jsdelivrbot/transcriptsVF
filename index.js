9ed7cfef4d5d4f4484d1ec047da966ce67b34a5a
marcodagolini <marco.dagolini@gmail.com>
2018-07-05 12:12:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1368,8 +1368,8 @@ app.get('/download', function(req, res) {
 		var j2xls = require('json2xls-xml')({ pretty : true });
  
 		var doc = {
-			{firstname : 'Foo', lastname: 'Bar', age: 23, weight: 25.7876, birth : new Date()},
-			{firstname : 'Bar', lastname: 'foobar', age: 73, weight: 22225.33, birth : new Date()}
+			[firstname : 'Foo', lastname: 'Bar', age: 23, weight: 25.7876, birth : new Date()],
+			[firstname : 'Bar', lastname: 'foobar', age: 73, weight: 22225.33, birth : new Date()]
 		};
 
 		var xls = j2xls(doc);
