209a26124a84e589a41726a9b4dc9875dea78ed0
marcodagolini <marco.dagolini@gmail.com>
2018-07-07 00:05:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,37 @@ var now = nowItsTime;
 
 
 
+function roughSizeOfObject( object ) {
+    var objectList = [];
+    var stack = [ object ];
+    var bytes = 0;
+    while ( stack.length ) {
+        var value = stack.pop();
+        if ( typeof value === 'boolean' ) {
+            bytes += 4;
+        }
+        else if ( typeof value === 'string' ) {
+            bytes += value.length * 2;
+        }
+        else if ( typeof value === 'number' ) {
+            bytes += 8;
+        }
+        else if
+        (
+            typeof value === 'object'
+            && objectList.indexOf( value ) === -1
+        )
+        {
+            objectList.push( value );
+            for( var i in value ) {
+                stack.push( value[ i ] );
+            }
+        }
+    }
+    return bytes;
+}
+
+
 
 /***************
 
@@ -1504,6 +1535,8 @@ app.get('/download', function(req, res) {
 			}
 			myFile = myFile + "\n";
 		}
+		
+		console.log(roughSizeOfObject(myFile));
 	
 
 		res.send("ok");
